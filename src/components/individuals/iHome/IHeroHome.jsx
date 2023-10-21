afe2786a90c9c29f98ff29ca5c21f9f91014ddd0
Bryan <bryansanudo95@gmail.com>
2023-10-20 19:26:54 -0500

    chat wpp

--- src/components/individuals/iHome/IHeroHome.jsx
+++ src/components/individuals/iHome/IHeroHome.jsx
@@ -3,16 +3,16 @@ import { Link } from "react-scroll";
 
 const messages = [
   "The Better FREE Way To Connect With The Right Manpower For Your Business",
-  "Unlock Your American Dream: Choose the EB3 Visa Path Today!",
-  "Why Settle for Less? Elevate Your Future with an EB3 Visa.",
-  "Open Doors in the U.S. – Make the EB3 Visa Your Key.",
-  "Step into a World of Opportunities: Get Your EB3 Visa Now.",
-  "EB3 Visa: Your Gateway to Permanent Residency and Beyond.",
-  "Discover the Fastest Path to Your American Life with an EB3 Visa.",
-  "Chart Your Course to Success: The EB3 Visa Awaits.",
-  "Be Part of America's Growth Story with an EB3 Visa.",
-  "Achieve the Unthinkable: U.S. Residency Through the EB3 Visa.",
-  "Your Journey to a Better Future Begins with an EB3 Visa.",
+  "Unlock Your American Dream: Choose the EB-3 Visa Path Today!",
+  "Why Settle for Less? Elevate Your Future with an EB-3 Visa.",
+  "Open Doors in the U.S. – Make the EB-3 Visa Your Key.",
+  "Step into a World of Opportunities: Get Your EB-3 Visa Now.",
+  "EB-3 Visa: Your Gateway to Permanent Residency and Beyond.",
+  "Discover the Fastest Path to Your American Life with an EB-3 Visa.",
+  "Chart Your Course to Success: The EB-3 Visa Awaits.",
+  "Be Part of America's Growth Story with an EB-3 Visa.",
+  "Achieve the Unthinkable: U.S. Residency Through the EB-3 Visa.",
+  "Your Journey to a Better Future Begins with an EB-3 Visa.",
   // Agrega más mensajes según tus necesidades
 ];
 
@@ -40,9 +40,6 @@ const IHeroHome = () => {
             <h1 className="my-5 lg:text-5xl md:text-3xl text-2xl font-bold tracking-wider">
               {randomMessage}
             </h1>
-            <p className="mb-5 md:text-3xl text-lg tracking-wider">
-              No Cost, No Hassle, No Brainer
-            </p>
 
             <Link to="teach me" smooth duration={500}>
               <button className="btn hover:scale-105 capitalize btn-primary tracking-wider">
