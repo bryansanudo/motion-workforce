84518be4570ea3cdbc6d164a935daaf935ba95cd
Bryan <bryansanudo95@gmail.com>
2023-11-01 12:53:50 -0500

    mobile icon scrol

--- src/components/individuals/iHome/IHeroHome.jsx
+++ src/components/individuals/iHome/IHeroHome.jsx
@@ -93,7 +93,12 @@ const IHeroHome = ({ language }) => {
             </a>
           </div>
           <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center cursor-pointer">
-            <Link to="teach me" smooth duration={500}>
+            <Link
+              to="teach me"
+              smooth
+              duration={500}
+              className="hidden sm:flex"
+            >
               <div className="w-[35px] h-[64px] rounded-3xl border-4 border-white flex justify-center items-start p-2">
                 <motion.div
                   animate={{
